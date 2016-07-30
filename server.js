e321f370318f5388eb21cc1406378155210e30f2
Marcelo Pasut <pasutmarcelo@gmail.com>
2016-07-30 17:32:09 -0300

    Adding config

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-/*
+
 var xnconfig = require('nodejsconfig');
 var fs = require('fs');
 var data = fs.readFileSync(__dirname+'/config/config.properties', 'UTF8');
 console.log('Scope: ' + process.env.NODE_ENV);
 
 config = xnconfig.parse(process.env.NODE_ENV, data);
-*/
+console.log('env: ' + config.env);
+
 var express = require('express');
 var app = express();
 
