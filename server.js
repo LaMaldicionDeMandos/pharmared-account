3ccb87175a55083ac63e22c3f741d26b6a23b83f
Marcelo Pasut <pasutmarcelo@gmail.com>
2016-07-30 17:22:34 -0300

    Add server app

---
+++ server.js
@@ -0,0 +1,22 @@
+/*
+var xnconfig = require('nodejsconfig');
+var fs = require('fs');
+var data = fs.readFileSync(__dirname+'/config/config.properties', 'UTF8');
+console.log('Scope: ' + process.env.NODE_ENV);
+
+config = xnconfig.parse(process.env.NODE_ENV, data);
+*/
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.get('/', function(req, res) {
+  res.send('success!!');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+
