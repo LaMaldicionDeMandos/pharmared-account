f34d277c6296ce5d5a9adaf6921885201c192a81
Marcelo Pasut <pasutmarcelo@gmail.com>
2016-07-31 18:09:06 -0300

    Uso strict false para soportar atributos especificos de cada entidad

--- model/entity_type.js
+++ model/entity_type.js
@@ -7,9 +7,8 @@ function EntityType() {
 
 EntityType.GOVERNMENTAL = new EntityType();
 EntityType.GOVERNMENTAL.toString = function() { return 'GOVERNMENTAL'};
-EntityType.GOVERNMENTAL.name = 'GOVERNMENTAL';
 EntityType.PHARMACY = new EntityType();
-EntityType.PHARMACY.name = 'PHARMACY';
+EntityType.PHARMACY.toString = function() { return 'PHARMACY'};
 
 EntityType.parse = function(name) { return EntityType[name];}
 
