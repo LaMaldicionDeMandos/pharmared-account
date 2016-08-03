0d197c3d690b32a59c3192f155eb1819eaf27460
Marcelo Pasut <pasutmarcelo@gmail.com>
2016-08-02 23:01:26 -0300

    Trabajo en registro de farmacia

--- services/register.js
+++ services/register.js
@@ -2,13 +2,19 @@
  * Created by boot on 7/31/16.
  */
 var q = require('q');
+var Address = require('../model/address');
+var Pharmacy = require('../model/pharmacy_entity');
+var validator = require('../model/model_validations');
 function RegisterService(db) {
     var ObjectId = db.ObjectId;
     var Entity = db.Entity;
     var User = db.User;
     var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     this.registerPharmacy = function(dto) {
-        var pharmacy = new Pharmacy(dt);
-        pharmacy._id = new Ob
+        var address = new Address(dto);
+        dto.address = address;
+        var pharmacy = new Pharmacy(dto);
+        //TODO me falta validar farmacia y usuario
+        var user = new User();
     };
 }
