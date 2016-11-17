182720fb517d20c474ef5b2620a720f8381c0b5a
Marcelo Pasut <pasutmarcelo@gmail.com>
2016-11-16 23:02:33 -0300

    Se activa el usuario cuando se cambia la password

--- services/user_service.js
+++ services/user_service.js
@@ -75,6 +75,7 @@ function UserService(db) {
                         def.reject('invalid_old');
                     } else {
                         user.password = sha(password);
+                        user.state = User.State.ACTIVE;
                         user.update(user, function(err, result) {
                             console.log(err + ' - ' + result);
                             if (!err) {
